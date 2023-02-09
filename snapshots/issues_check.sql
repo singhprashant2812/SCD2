bcac99330d2de91fae88f9464931ed00239923d9
singhprashant2812@gmail.com <singhprashant2812@gmail.com>
2023-02-09 14:03:09 +0000

    updated

---
+++ snapshots/issues_check.sql
@@ -0,0 +1,15 @@
+{% snapshot issues_snapshot %}
+
+{{
+    config(
+        target_database = dbt,
+        unique_key="ticket_id", 
+        strategy="check", 
+        check_cols="all", 
+        transient=false
+    )
+}} 
+
+select * from dit_poc.issues
+
+{% endsnapshot %}
